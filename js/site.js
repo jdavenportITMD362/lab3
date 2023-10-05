7994408a956687e716257232e805ce96e310f138
Joshua Davenport <87725934+jdavenport2021@users.noreply.github.com>
2023-10-05 14:08:37 -0500

    Custom checkbox

--- js/site.js
+++ js/site.js
@@ -2,4 +2,24 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  $('#alt-checkbox').on("click", function () {
+  	if ($('#age-verification').is(':checked')) {
+  		$('#age-verification').prop("checked", false);
+  		$('#alt-checkbox').html("<p></p>");
+  	}
+  	else {
+	  	$('#age-verification').prop("checked", true);
+	  	$('#alt-checkbox').html("<p>&#10004;</p>");
+  	}
+  });
+  $('#forAge').on("click", function () { // For some reason for ckicking on the label to work the way it should the checked values need to be flipped?
+  	if ($('#age-verification').is(':checked')) {
+			$('#age-verification').prop("checked", true);
+			$('#alt-checkbox').html("<p></p>");
+  	}
+  	else {
+			$('#age-verification').prop("checked", false);
+			$('#alt-checkbox').html("<p>&#10004;</p>");
+		}
+  });
 });
